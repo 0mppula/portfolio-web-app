0a3f6dc0c0d79248b75b2a1bf7175d87a82c94be
Omar Kraidie <omppula_1@hotmail.com>
2022-06-12 12:53:12 +0300

    added rel attrbute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
 				<a href="https://github.com/0mppula" target="_blank" rel="noreferrer">
 					<FontAwesomeIcon className="footer-icon icon" icon={faGithub} />
 				</a>
-				<a href="https://www.linkedin.com/in/omar-kraidi%C3%A9-b7a8141a8/" target="_blank">
+				<a href="https://www.linkedin.com/in/omar-kraidi%C3%A9-b7a8141a8/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon className="footer-icon icon" icon={faLinkedin} />
 				</a>
 				<a href="https://twitter.com/OmarKraidie" target="_blank" rel="noreferrer">
