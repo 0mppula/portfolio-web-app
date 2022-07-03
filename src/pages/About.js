8e5398fd9483572aedf1bc4fa1aabe44456ccbb0
Omar Kraidie <omppula_1@hotmail.com>
2022-07-03 11:40:24 +0300

    updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,32 +19,55 @@ const About = () => {
 					<div className="about-text">
 						<h1>Software Engineering</h1>
 						<div className="about-info">
-							<p>
-								My name is <b>Omar <span className="secondary-color">Kraidié</span></b>. I'm a second year software engineering student
-								currently studying in LAB University of Applied Sciences in Lahti!
-							</p>
-							<p>
-								I wrote my first hello world app with HTML & CSS in 2019 when
-								programming was first introduced to me in an introductionary
-								programming course in vocational school.
-							</p>
-							<p>
-								I Started seariously pursuing my coding career in early 2020 on my
-								last year of IT-vocational school. I began with self-learning more
-								complex HTML & CSS, and quickly advancing to JavaScript which is my
-								favourite programming language today 🙂.
-							</p>
-							<p>
-								In 2021 after learning the ropes with JS, and creating a few
-								projects with the language. I started my Bachelor's degree in
-								Computer Science at my local University LAB.
-							</p>
-							<p>
-								On January 2022, after my 4 month long web-dev internship role at my
-								local start-up Tavata Global Oy, I got my first <b>real</b> part-time web-dev job at the
-								company.
-							</p>
-							<Frame />
+							<div>
+								<p>
+									My name is{' '}
+									<b>
+										Omar <span className="secondary-color">Kraidié</span>
+									</b>
+									. I'm a second year software engineering student currently
+									studying in LAB University of Applied Sciences in Lahti!
+								</p>
+							</div>
+							
+							<div>
+								<p>Milestones</p>
+								<ul>
+									<li>
+										<b>Q3 2019</b> Wrote my first Hello World! web-app.
+									</li>
+									<li>
+										<b>Q1 2020</b> Decided to seariously pursue software
+										engineering as a full-time career.
+									</li>
+									<li>
+										<b>Q4 2020</b> Developed first my couple of projects with
+										HTML, CSS & JavaScript.
+									</li>
+									<li>
+										<b>Q1 2021</b> Started Bachelor's degree in Computer Science
+										at my local University LAB.
+									</li>
+									<li>
+										<b>Q2 2021</b> Decided to start learning Python & use it as
+										my <b>secondary</b> programming language.
+									</li>
+									<li>
+										<b>Q3 2021</b> Began internship at Tavata Global Oy
+										(web-development).
+									</li>
+									<li>
+										<b>Q1 2022</b> Started first <b>real</b> part-time web-dev
+										job at Tavata Global Oy.
+									</li>
+									<li>
+										<b>Q2 2022</b> Started <b>second</b> part-time web-dev job
+										at Ovio Carbook Oy.
+									</li>
+								</ul>
+
+								<Frame />
+							</div>
 						</div>
 					</div>
 				</div>
