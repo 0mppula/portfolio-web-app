eef243f3482c6e332e0af4ace15aa3c458142e1b
Omar Kraidie <omppula_1@hotmail.com>
2023-03-12 17:03:21 +0200

    Refactored project images directory.

--- src/data/project-thumbnails.js
+++ src/data/project-thumbnails.js
@@ -1,15 +1,15 @@
 import placeholderThumbnail from '../images/projects/project_bg.jpg';
-import KoronKorko from '../images/projects/koronkorko.jpg';
-import CoinCaps from '../images/projects/coincaps.jpg';
-import wsbTickers from '../images/projects/wsb-tickers.jpg';
-import tsMultiPageForm from '../images/projects/ts-multi-page-form.jpg';
-import tsPaginatedUsersPage from '../images/projects/ts-paginated-users-page.jpg';
-import reactReduxToolkitHelloWorld from '../images/projects/react-redux-toolkit-hello-world.jpg';
-import DejaVu from '../images/projects/DejaVu.jpg';
-import CSGO_Weapons from '../images/projects/CS-GO_weapons.jpg';
-import Polish_Cow from '../images/projects/Polish_Cow.jpg';
-import SimpleJavaScriptProjects from '../images/projects/SimpleJavaScriptProjects.png';
-import Alien_Invasion from '../images/projects/Alien_Invasion.jpg';
+import KoronKorko from '../images/projects/KoronKorko/koronkorko.jpg';
+import CoinCaps from '../images/projects/CoinCaps/coincaps.jpg';
+import wsbTickers from '../images/projects/WSB-Tickers/wsb-tickers.jpg';
+import tsMultiPageForm from '../images/projects/ts-multi-page-form/ts-multi-page-form.jpg';
+import tsPaginatedUsersPage from '../images/projects/ts-paginated-users-page/ts-paginated-users-page.jpg';
+import reactReduxToolkitHelloWorld from '../images/projects/react-redux-toolkit-hello-world//react-redux-toolkit-hello-world.jpg';
+import DejaVu from '../images/projects/DejaVu/DejaVu.jpg';
+import CSGO_Weapons from '../images/projects/CS-GO_weapons/CS-GO_weapons.jpg';
+import Polish_Cow from '../images/projects/Polish_Cow/Polish_Cow.jpg';
+import SimpleJavaScriptProjects from '../images/projects/SimpleJavaScriptProjects/SimpleJavaScriptProjects.png';
+import Alien_Invasion from '../images/projects/Alien_Invasion/Alien_Invasion.jpg';
 
 const thumbnails = {
 	KoronKorko: KoronKorko,
