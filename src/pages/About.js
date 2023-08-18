e343f1cabb20523c29ed7cf8461a7c86ffa377e9
Omar Kraidié <omppula_1@hotmail.com>
2023-08-18 11:01:20 +0300

    Clean up.

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,106 +41,107 @@ const About = () => {
 					<h1>A Little About Me</h1>
 					<div className="header-underline"></div>
 				</div>
-			</div>
 
-			<div className="about-content-container">
-				<div className="about-content">
-					<div className="about-image" />
-				</div>
-
-				<div className="about-content">
-					<h2>General Info</h2>
-					<div>
-						<p>
-							My name is <b>Omar Kraidié</b> I'm a third-year software engineering
-							student with over 2 years of professional experience in developing
-							client-facing projects for businesses, as well as 3+ years of experience
-							developing independent open-source projects.
-							<br />
-							<br />I like to create useful software that can be leveraged in everyday
-							applications. Furthermore, I'm enthusiastic about learning how to use
-							new technologies and finding efficient ways of implementing modern
-							design patterns. In short, if you have a software problem that needs to
-							be resolved I <b>will</b> develop the perfect and sustainable solution
-							for you.
-							<br />
-							<br />
-							In addition to coding, I'm interested in investing, economics,
-							artificial intelligence and personal development.
-							<br />
-							<br />
-							If you're interested, here's a full list of the{' '}
-							<a
-								href="https://www.goodreads.com/review/list/135003326-0mppu?ref=nav_mybooks&shelf=programming"
-								target="_blank"
-								rel="noreferrer"
-								className="link"
-							>
-								tech books
-							</a>{' '}
-							that I've read.
-						</p>
-						<Frame />
+				<div className="about-content-container">
+					<div className="about-content">
+						<div className="about-image" />
 					</div>
-				</div>
 
-				<div className="about-content">
-					<h2>My Tech Stack</h2>
-					<div className="project-tech-icons">
-						{[...technologies].sort().map((technology) => (
-							<React.Fragment key={technology}>
-								<span
-									data-tooltip-id={`${technology}-tooltip`}
-									data-tooltip-content={technology}
+					<div className="about-content">
+						<h2>General Info</h2>
+						<div>
+							<p>
+								My name is <b>Omar Kraidié</b> I'm a third-year software engineering
+								student with over 2 years of professional experience in developing
+								client-facing projects for businesses, as well as 3+ years of
+								experience developing independent open-source projects.
+								<br />
+								<br />I like to create useful software that can be leveraged in
+								everyday applications. Furthermore, I'm enthusiastic about learning
+								how to use new technologies and finding efficient ways of
+								implementing modern design patterns. In short, if you have a
+								software problem that needs to be resolved I <b>will</b> develop the
+								perfect and sustainable solution for you.
+								<br />
+								<br />
+								In addition to coding, I'm interested in investing, economics,
+								artificial intelligence and personal development.
+								<br />
+								<br />
+								If you're interested, here's a full list of the{' '}
+								<a
+									href="https://www.goodreads.com/review/list/135003326-0mppu?ref=nav_mybooks&shelf=programming"
+									target="_blank"
+									rel="noreferrer"
+									className="link"
 								>
-									{getTechnologyIcon(technology)}
-								</span>
-
-								<Tooltip id={`${technology}-tooltip`} />
-							</React.Fragment>
-						))}
-						<Frame />
+									tech books
+								</a>{' '}
+								that I've read.
+							</p>
+							<Frame />
+						</div>
 					</div>
-				</div>
 
-				<div className="about-content">
-					<h2>Career Milestones</h2>
-					<div>
-						<ul>
-							<li>
-								<b>Q3 2019</b> Wrote my first "Hello World!" project. 🌍
-							</li>
-							<li>
-								<b>Q1 2020</b> Decided to pursue a full-time career in software
-								engineering.
-							</li>
-							<li>
-								<b>Q2 2020</b> Developed projects using HTML, CSS, and JavaScript.
-								👨🏼‍💻
-							</li>
-							<li>
-								<b>Q1 2021</b> Enrolled in Bachelor's degree in Computer Science. 🏫
-							</li>
-							<li>
-								<b>Q2 2021</b> Learned Python and data science. 📊
-							</li>
-							<li>
-								<b>Q3 2021</b> Interned at Tavata Global Oy (web development).
-							</li>
-							<li>
-								<b>Q1 2022</b> Started part-time web developer role at Tavata Global
-								Oy.
-							</li>
-							<li>
-								<b>Q2 2022</b> Started part-time web developer role at Ovio Carbook
-								Oy.
-							</li>
-							<li>
-								<b>Q4 2022</b> Started full-time web developer role at twoday
-								Finland.
-							</li>
-						</ul>
-						<Frame />
+					<div className="about-content">
+						<h2>My Tech Stack</h2>
+						<div className="project-tech-icons">
+							{[...technologies].sort().map((technology) => (
+								<React.Fragment key={technology}>
+									<span
+										data-tooltip-id={`${technology}-tooltip`}
+										data-tooltip-content={technology}
+									>
+										{getTechnologyIcon(technology)}
+									</span>
+
+									<Tooltip id={`${technology}-tooltip`} />
+								</React.Fragment>
+							))}
+							<Frame />
+						</div>
+					</div>
+
+					<div className="about-content">
+						<h2>Career Milestones</h2>
+						<div>
+							<ul>
+								<li>
+									<b>Q3 2019</b> Wrote my first "Hello World!" project. 🌍
+								</li>
+								<li>
+									<b>Q1 2020</b> Decided to pursue a full-time career in software
+									engineering.
+								</li>
+								<li>
+									<b>Q2 2020</b> Developed projects using HTML, CSS, and
+									JavaScript. 👨🏼‍💻
+								</li>
+								<li>
+									<b>Q1 2021</b> Enrolled in Bachelor's degree in Computer
+									Science. 🏫
+								</li>
+								<li>
+									<b>Q2 2021</b> Learned Python and data science. 📊
+								</li>
+								<li>
+									<b>Q3 2021</b> Interned at Tavata Global Oy (web development).
+								</li>
+								<li>
+									<b>Q1 2022</b> Started part-time web developer role at Tavata
+									Global Oy.
+								</li>
+								<li>
+									<b>Q2 2022</b> Started part-time web developer role at Ovio
+									Carbook Oy.
+								</li>
+								<li>
+									<b>Q4 2022</b> Started full-time web developer role at twoday
+									Finland.
+								</li>
+							</ul>
+							<Frame />
+						</div>
 					</div>
 				</div>
 			</div>
