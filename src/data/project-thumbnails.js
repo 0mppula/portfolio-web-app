fb51ff6ccd72db04a5529cc2f837237a5ab3d0c1
Omar Kraidié <omppula_1@hotmail.com>
2024-01-18 14:45:54 +0200

    Updated project thumbnails.

--- src/data/project-thumbnails.js
+++ src/data/project-thumbnails.js
@@ -1,37 +1,39 @@
-import placeholderThumbnail from '../images/projects/project_bg.jpg';
-import KoronKorko from '../images/projects/KoronKorko/koronkorko.png';
-import CoinCaps from '../images/projects/CoinCaps/coincaps.png';
+import alienInvasion from '../images/projects/Alien_Invasion/Alien_Invasion.png';
+import bookStackr from '../images/projects/BookStackr/BookStackr.png';
+import CSGOWeapons from '../images/projects/CS-GO_weapons/CS-GO_weapons.png';
+import coinCaps from '../images/projects/CoinCaps/coincaps.png';
+import dejaVu from '../images/projects/DejaVu/DejaVu.png';
+import koronKorko from '../images/projects/KoronKorko/koronkorko.png';
+import polishCow from '../images/projects/Polish_Cow/Polish_Cow.png';
+import simpleJavaScriptProjects from '../images/projects/SimpleJavaScriptProjects/SimpleJavaScriptProjects.png';
 import wsbTickers from '../images/projects/WSB-Tickers/wsb-tickers.png';
+import elementCompare from '../images/projects/element-compare/element-compare.png';
+import goalTrack from '../images/projects/goal-trackr/goal-trackr.png';
+import moneyMapper from '../images/projects/money-mapper/money-mapper.png';
+import placeholderThumbnail from '../images/projects/project_bg.jpg';
+import reactReduxToolkitHelloWorld from '../images/projects/react-redux-toolkit-hello-world/react-redux-toolkit-hello-world.png';
 import tsMultiPageForm from '../images/projects/ts-multi-page-form/ts-multi-page-form.png';
 import tsPaginatedUsersPage from '../images/projects/ts-paginated-users-page/ts-paginated-users-page.png';
-import reactReduxToolkitHelloWorld from '../images/projects/react-redux-toolkit-hello-world/react-redux-toolkit-hello-world.png';
-import DejaVu from '../images/projects/DejaVu/DejaVu.png';
-import CSGO_Weapons from '../images/projects/CS-GO_weapons/CS-GO_weapons.png';
-import Polish_Cow from '../images/projects/Polish_Cow/Polish_Cow.png';
-import SimpleJavaScriptProjects from '../images/projects/SimpleJavaScriptProjects/SimpleJavaScriptProjects.png';
-import Alien_Invasion from '../images/projects/Alien_Invasion/Alien_Invasion.png';
-import BookStackr from '../images/projects/BookStackr/BookStackr.png';
-import GoalTrack from '../images/projects/goal-trackr/goal-trackr.png';
-import MoneyMapper from '../images/projects/money-mapper/money-mapper.png';
-import ViewportSelect from '../images/projects/viewport-select/viewport-select.png';
+import viewportSelect from '../images/projects/viewport-select/viewport-select.png';
 
 const thumbnails = {
-	'money-mapper': MoneyMapper,
-	KoronKorko: KoronKorko,
-	CoinCaps: CoinCaps,
-	BookStackr: BookStackr,
+	'money-mapper': moneyMapper,
+	KoronKorko: koronKorko,
+	CoinCaps: coinCaps,
+	BookStackr: bookStackr,
 	'ts-paginated-users-page': tsPaginatedUsersPage,
-	DejaVu: DejaVu,
+	DejaVu: dejaVu,
 	'react-redux-toolkit-hello-world': reactReduxToolkitHelloWorld,
 	'wsb-tickers': wsbTickers,
 	'ts-multi-page-form': tsMultiPageForm,
-	Polish_Cow: Polish_Cow,
-	'CS-GO_weapons': CSGO_Weapons,
-	Alien_Invasion: Alien_Invasion,
-	SimpleJavaScriptProjects: SimpleJavaScriptProjects,
-	'goal-trackr': GoalTrack,
-	'viewport-select': ViewportSelect,
+	Polish_Cow: polishCow,
+	'CS-GO_weapons': CSGOWeapons,
+	Alien_Invasion: alienInvasion,
+	SimpleJavaScriptProjects: simpleJavaScriptProjects,
+	'goal-trackr': goalTrack,
+	'viewport-select': viewportSelect,
 	placeholder: placeholderThumbnail,
+	'element-compare': elementCompare,
 };
 
 export default thumbnails;
