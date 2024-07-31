2a8fa9fac83ad81083d339e944e175f46faef43f
Omar Kraidié <omppula_1@hotmail.com>
2024-07-31 13:34:28 +0300

    Styled new landing page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,92 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import { FaEnvelope } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Frame from '../components/Frame';
 import { useTitle } from '../hooks/useTitle';
+import hero from '../images/hero.png';
 
 const Home = () => {
 	useTitle('home');
 	return (
 		<div className="hero-container">
-			<div className="greet-card">
-				<h1>
-					<span>Hello 👋🏼,</span>
-					<span>I'm Omar</span>
-				</h1>
+			<div>
+				<div className="secondary-avatar">
+					<img src={hero} alt="Omar Kraidié"></img>
+				</div>
 
-				<div className="greet-bottom">
-					<div className="greet-info">
-						<p>
-							I'm a full-stack software engineer based in Finland. I like developing
-							cool and useful software by leveraging modern technologies!
-						</p>
+				<h1>Fullstack Developer, Omar Kraidié 👋🏼</h1>
 
-						<p>
-							Feel free to view some of my showcased projects and their code.
-							Additionally, you can take a look at my CV or reach out to me via email
-							by clicking the buttons below.
-						</p>
-						<Frame />
-					</div>
+				<div>
+					{/* Greet card */}
+					<div className="greet-card">
+						<div>
+							<p>
+								I'm a fullstack developer based in Finland. I like developing cool
+								and useful software by leveraging modern technologies!
+							</p>
 
-					<div className="greet-buttons">
-						<Link to="/projects" type="button" className="btn" aria-label="projects">
-							Projects
-						</Link>
+							<p>
+								Feel free to view some of my showcased projects and their code.
+								Additionally, you can take a look at my CV or reach out to me via
+								email by clicking the buttons below.
+							</p>
+							<Frame />
+						</div>
 
-						<Link
-							to="/cv"
-							target="_blank"
-							rel="noreferrer"
-							type="button"
-							className="btn"
-							aria-label="CV"
-						>
-							CV (.PDF)
-						</Link>
+						<div className="greet-buttons">
+							<Link
+								to="/projects"
+								type="button"
+								className="btn"
+								aria-label="projects"
+							>
+								Projects
+							</Link>
 
-						<a
-							href="mailto:devomarkraidie@gmail.com"
-							target="_blank"
-							rel="noreferrer"
-							type="button"
-							className="btn"
-							aria-label="Email"
-							title="Send me an email!"
-							style={{
-								display: 'flex',
-								justifyContent: 'center',
-								alignItems: 'center',
-								fontSize: '1.375rem',
-							}}
-						>
-							<FaEnvelope className="icon" />
-						</a>
+							<Link
+								to="/cv"
+								target="_blank"
+								rel="noreferrer"
+								type="button"
+								className="btn"
+								aria-label="CV"
+							>
+								CV (.PDF)
+							</Link>
+
+							<a
+								href="mailto:devomarkraidie@gmail.com"
+								target="_blank"
+								rel="noreferrer"
+								type="button"
+								className="btn"
+								aria-label="Email"
+								title="Send me an email!"
+								style={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center',
+									fontSize: '1.375rem',
+								}}
+							>
+								<FaEnvelope className="icon" />
+							</a>
+						</div>
+
+						<div className="greet-card-stars">
+							<div>
+								{[...Array.from({ length: 5 })].map((_, i) => (
+									<img key={i} src="" alt="" />
+								))}
+							</div>
+
+							<p>Github Stargazers</p>
+						</div>
 					</div>
 				</div>
 			</div>
+
+			<img src={hero} alt="Omar Kraidié" />
 		</div>
 	);
 };
